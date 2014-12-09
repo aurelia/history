e8d7dc317d1b1cdd954d39c1a63019030985b164
Rob Eisenberg <rob@bluespire.com>
2014-12-09 14:25:30 -0500

    chore(*): setup the project structure and build
    This commit adds the initial source code, sets up the project
    structure, creates the gulp build file and configures karma.

---
+++ dist/commonjs/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var History = (function () {
+  var History = function History() {};
+
+  History.prototype.activate = function () {
+    throw new Error("History must implement activate().");
+  };
+
+  History.prototype.deactivate = function () {
+    throw new Error("History must implement deactivate().");
+  };
+
+  History.prototype.navigate = function () {
+    throw new Error("History must implement navigate().");
+  };
+
+  History.prototype.navigateBack = function () {
+    throw new Error("History must implement navigateBack().");
+  };
+
+  return History;
+})();
+
+exports.History = History;
